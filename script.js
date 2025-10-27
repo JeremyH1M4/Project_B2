6a802bd4cea8c5d5a202f5530e8f1fd707b6aa9c
jeremhuy28 <jeremhuy28@g.crookcountyschools.org>
2025-10-27 14:45:16 -0700

    test_js

---
+++ script.js
@@ -0,0 +1,13 @@
+
+//testing timer function
+let timeLeft = 10; {
+    N1 = prompt("Enter your name");
+    if (timeLeft > 0 || N1 ) {
+        alert("Hello" + " " + N1);
+    }
+
+    else (timeLeft <= 0); {
+        alert("Time ran out")
+    }
+    
+}
