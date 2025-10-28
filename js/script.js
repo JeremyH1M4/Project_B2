f675eaf337fa776ef931e1e97c476452594fef71
isaacness26 <isaacness26@g.crookcountyschools.org>
2025-10-28 15:11:05 -0700

    changed js

--- js/script.js
+++ js/script.js
@@ -2,18 +2,6 @@
 'use strict';
 //testing timer function
 const N1 = prompt("Enter your name"); 
-let timeLeft = 10; {
-    
-
-    if (timeLeft > 0 || N1 ) {
-        alert("Hello" + " " + N1);
-    }
-
-    else (timeLeft <= 0); {
-        alert("Time ran out")
-    }
-    
-};
 
 document.write("<p>Welcome to our start page " + N1 + "! Please click start when you're ready.</p>");
 
