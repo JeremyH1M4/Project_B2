7a847612824d0ff8e5efe77a4b73f3082c8552ed
isaacness26 <isaacness26@g.crookcountyschools.org>
2025-10-28 15:23:31 -0700

    changes

--- js/script.js
+++ js/script.js
@@ -2,9 +2,20 @@
 'use strict';
 //testing timer function
 const N1 = prompt("Enter your name"); 
+let timeLeft = 10; {
+    
+
+    if (timeLeft > 0 || N1 ) {
+        alert("Hello" + " " + N1);
+    }
+
+    else (timeLeft <= 0); {
+        alert("Time ran out")
+    }
+    
+};
 
 document.write("<p>Welcome to our start page " + N1 + "! Please click start when you're ready.</p>");
 
-console.log("JavaScript is linked properly");
-
+console.log("JavaScript is linked properly")
 
