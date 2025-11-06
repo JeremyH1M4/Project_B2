ddb35c2f3babeb2db6a52792570d84e96925dea6
jeremhuy28 <jeremhuy28@g.crookcountyschools.org>
2025-11-06 11:06:03 -0800

    bug fixes

--- js/Page-UnknownM4.js
+++ js/Page-UnknownM4.js
@@ -1,6 +1,6 @@
 let buttonM5 = document.getElementById("start4");
      if (buttonM5) 
          buttonM5.addEventListener("click", function() {
-            alert("It looks comforting. But something feels off, doesn't it? I won't go there if I was you.");
+            alert("It looks comforting. But something feels off, doesn't it? I won't go there if I were you.");
             window.location.href = "mystery2.html";
          })
